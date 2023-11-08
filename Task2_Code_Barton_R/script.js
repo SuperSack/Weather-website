f4dedd8a62a3826e0351f955b32c3a4517ddd16d
Reid <113024538+SuperSack@users.noreply.github.com>
2023-11-08 11:45:56 +0000

    Add files via upload

--- Task2_Code_Barton_R/script.js
+++ Task2_Code_Barton_R/script.js
@@ -1,17 +1,23 @@
 let weather ={
-    "api key": "0ec023cda580faf1f85b38ec7416d270",
+    "apiKey": "0ec023cda580faf1f85b38ec7416d270",
     fetchWeather: function (city) {
         fetch("https://api.openweathermap.org/data/2.5/weather?q=" 
         + city 
         + "&units=metric&appid=" 
-        + appKey)
+        + this.apiKey
+        )
         .then((response) => response.json())
-        .then((data) => console.log(data));
+        .then((data) => this.displayWeather(data));
     },
     displayWeather: function(data) {
-
+        const{ name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        console.log(name,icon,description,temp,humidity,speed)
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + "@2x.png"
     }
 };
 
-
-21:49
+26:10
