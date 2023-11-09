421a9c6ce3c70878a04035642d8bc322510a36ab
Reid <113024538+SuperSack@users.noreply.github.com>
2023-11-09 10:22:04 +0000

    Add files via upload

--- Task2_Code_Barton_R/script.js
+++ Task2_Code_Barton_R/script.js
@@ -14,10 +14,30 @@ let weather ={
         const { icon, description } = data.weather[0];
         const { temp, humidity } = data.main;
         const { speed } = data.wind;
-        console.log(name,icon,description,temp,humidity,speed)
         document.querySelector(".city").innerText = "Weather in " + name;
-        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + "@2x.png"
+        document.querySelector(".icon").src =
+         "https://openweathermap.org/img/wn/" + icon + ".png"
+         document.querySelector(".description").innerText = description;
+         document.querySelector(".temp").innerText = temp + "°C";
+         document.querySelector(".humidity").innerText = "humidity: " + humidity + "%";
+         document.querySelector(".wind").innerText =
+          "Wind speed: " + speed + "mph";
+          document.querySelector(".weather").classList.remove("loading");
+    },
+    search: function () {
+        this.fetchWeather(document.querySelector(".search-bar").value);
     }
 };
 
-26:10
+document.querySelector(".search button").addEventListener("click", function () {
+    weather.search();
+
+})
+
+document.querySelector(".search-bar").addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+        weather.search();
+    }
+})
+
+weather.fetchWeather("Norwich")
